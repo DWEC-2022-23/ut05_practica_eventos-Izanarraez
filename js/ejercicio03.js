a6c9079e8f337338462232dd964eface63b51a95
Izan Arraez <izanarraez@gmail.com>
2022-10-27 18:34:49 +0200

    a

---
+++ js/ejercicio03.js
@@ -0,0 +1,15 @@
+/*function mousemov(event){
+    var px = event.paginaX;
+    var py = event.paginaY;
+    var nx = event.clientX;
+    var ny = event.clientY
+
+    let resPag = "Pagina["+px+","+py+"]";
+    let resnav = "Navegador["+
+    document.getElementById("mRaton").innerHTML;
+}
+
+document.getElementById("info").onmousemove = mousemov(evento);*/
+
+
+
